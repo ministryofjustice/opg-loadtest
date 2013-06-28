c460459454c0bb7c1563a84b189b42909c0be7c6
Mateusz Łapsa-Malawski <mateusz@munhitsu.com>
2013-06-28 09:52:05 +0100

    snapshot of tsung scenarios

---
+++ tsung/fabfile.py
@@ -0,0 +1,13 @@
+from fabric.api import task, run, env, local
+from fabric.contrib.project import rsync_project
+
+@task
+def tsung(test_name):
+    rsync_project("tsung/", "scenarios/", delete=True)
+    run("mkdir -p log")
+    run("tsung -l log -f tsung/{} start".format(test_name))
+
+    local("rsync  -pthrvz  --rsh='ssh  -p 22 ' {user}@{host}:log/ log/".format(user=env.user,host=env.host_string))
+
+    print "remember to use:"
+    print "/usr/local/lib/tsung/bin/tsung_stats.pl"
