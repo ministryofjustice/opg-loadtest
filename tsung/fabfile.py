27f7e2c79dddc81c319b83c74fc5e6702fd21733
Mateusz Łapsa-Malawski <mateusz@munhitsu.com>
2013-06-28 21:40:38 +0100

    added foll walk through

--- tsung/fabfile.py
+++ tsung/fabfile.py
@@ -4,10 +4,14 @@ from fabric.contrib.project import rsync_project
 @task
 def tsung(test_name):
     rsync_project("tsung/", "scenarios/", delete=True)
-    run("mkdir -p log")
-    run("tsung -l log -f tsung/{} start".format(test_name))
+    run("mkdir -p results")
+    run("tsung -l results -f tsung/{} start".format(test_name))
 
-    local("rsync  -pthrvz  --rsh='ssh  -p 22 ' {user}@{host}:log/ log/".format(user=env.user,host=env.host_string))
+    local("rsync  -pthrvz  --rsh='ssh  -p 22 ' {user}@{host}:results/ results/".format(user=env.user, host=env.host_string))
 
     print "remember to use:"
     print "/usr/local/lib/tsung/bin/tsung_stats.pl"
+
+@task
+def tsung_download_records():
+    local("rsync  -pthrvz  --rsh='ssh  -p 22 ' {user}@{host}:.tsung/ log/".format(user=env.user, host=env.host_string))
